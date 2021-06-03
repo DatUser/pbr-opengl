74c8c61045d9d9f7e0f35fa5b557583da348fbdf
Xavier Gomez <xavier.gomez@epita.fr>
2021-06-03 17:02:58 +0200

    [UPDATE] Finished part 1

--- src/matrix.cc
+++ src/matrix.cc
@@ -10,6 +10,27 @@ namespace mygl
     array = values;
   }
 
+  matrix4 matrix4::operator*(const matrix4& rhs) const
+  {
+    std::vector<float> out_values(16, 0);
+
+    for (unsigned int i = 0; i < 4; i++)
+    {
+      for (unsigned int j = 0; j < 4; j++)
+      {
+        float acc = 0; 
+        for (unsigned int k = 0; k < 4; k++)
+        {
+          acc += array[i * 4 + k] + rhs[k * 4 + j];
+        }
+
+        out_values[i * 4 + j] = acc;
+      }
+    }
+
+    return matrix4(out_values);
+  }
+
   void matrix4::operator*=(const matrix4& rhs)
   {
     for (unsigned int i = 0; i < 4; i++)
